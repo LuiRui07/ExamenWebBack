f6136e05f8f03a02745b9d11c277f3881330d827
Luis Ruiz <100932068+LuiRui07@users.noreply.github.com>
2023-12-14 14:36:46 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 const express = require('express');
 const cors = require("cors");
 const mongoose = require("mongoose")
-require("dotenv").config({ path: "./config.env" });
+
 const app = express();
-const port = process.env.PORT;
+const port = 5001;
 app.use(express.json());
 app.use(cors());
 
 const routes = require("./routes.js")
 app.use('/api', routes);
 mongoose.connect(
-  process.env.ATLAS_URI).then(()=>
+  "mongodb+srv://ei:ei@cluster0.1acabfy.mongodb.net/kalendas").then(()=>
     console.log("Hemos conectado con mongoDB")
   ).catch((error)=>
     console.error(error)
